d733960189e0bdd93ab53a740cfed7e27ec201ee
PhanThiThuan <phanthuanphan2912@gmail.com>
2020-10-02 18:48:25 +0700

    complete

---
+++ app/src/main/java/com/example/customlist/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.customlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+
+    String [] numberWord = {"one","two","three","four","five","six","seven","eight","nine","ten"};
+    int [] numberImage = {R.drawable.one,R.drawable.two,R.drawable.three,R.drawable.four,R.drawable.five,R.drawable.six,R.drawable.seven,R.drawable.eight,R.drawable.nine,R.drawable.ten};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.list_view);
+        MainAdapter adapter = new MainAdapter (MainActivity.this,numberWord,numberImage);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                String name;
+                intent.putExtra("image",numberImage[position]);
+                intent.putExtra("word",numberImage[position]);
+                startActivity(intent);
+            }
+        });
+    }
+}
